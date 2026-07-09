6b3c27f30436a83a8a60a448efde03569e063506
kurtmetzger <kurtametzger@gmail.com>
2026-07-09 18:45:56 -0500

    adjusted responsiveness of AlbumCard.jsx for consistency

--- src/components/AlbumCard.jsx
+++ src/components/AlbumCard.jsx
@@ -5,14 +5,14 @@ function AlbumCard({ album, index }){
         <div className="albumCard">
             <div className='albumInfo'>
                 <h5>{index + 1}.</h5>
-                <img src={album.coverArt || '/no_album_art.png'} alt={album.albumName} />
+                <img className='albumArt' src={album.coverArt || '/no_album_art.png'} alt={album.albumName} />
                 <div className='albumDetails'>
-                    <div className='albumtitleWrapper'>
-                        <h4 className='albumTitle'>{album.albumName}</h4>
+                    <div className='albumTitleWrapper'>
+                        <h3 className='albumTitle'>{album.albumName}</h3>
                     </div>
-                    <h5>{album.artist}</h5>
-                    <p>{album.engagedTracks}/{album.totalTracks} tracks listened</p>
-                    <h5>Match score: {Math.round(album.score *100) / 100}</h5>
+                    <h4>{album.artist}</h4>
+                    <h4>{album.engagedTracks}/{album.totalTracks} tracks listened</h4>
+                    <h4>Match score: {Math.round(album.score *100) / 100}</h4>
                 </div>
             </div>
             <div className="purchaseButtons">
